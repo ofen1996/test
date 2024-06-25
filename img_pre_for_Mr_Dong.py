fc2c279af142d2086854af1a3ac3f8236c5771fe
ofen <526083628@qq.com>
2024-06-25 10:41:40 +0800

    保存

--- img_pre_for_Mr_Dong.py
+++ img_pre_for_Mr_Dong.py
@@ -10,11 +10,13 @@ def convert16_2_8(img_path, save_path=None):
     if save_path is None:
         save_path = os.path.splitext(img_path)[0] + "-8bit.tif"
 
-    # img_16 = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-    img_16 = np.asarray(Image.open(img_path))
+    img_16 = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
+    # img_16 = np.asarray(Image.open(img_path))
     img_8bit = cv2.convertScaleAbs(img_16, alpha=(255.0/img_16.max()))
     # tifffile.imwrite(save_path, img_8bit, compression='LZW')
-    return img_8bit
+    img_8bit = cv2.cvtColor(img_8bit, cv2.COLOR_RGB2GRAY)
+    img_8bit = cv2.convertScaleAbs(img_8bit, alpha=(255.0 / img_8bit.max()))
+    return img_8bit.astype(np.uint8)
 
 def merge_gray_to_RGB(imgs_dir):
     img_names = os.listdir(imgs_dir)
@@ -22,11 +24,14 @@ def merge_gray_to_RGB(imgs_dir):
         raise Exception(f"Images num > 3 or ==0, check dir: {imgs_dir}")
     img_names.sort()
 
-    img_0 = convert16_2_8(os.path.join(imgs_dir, img_names[0]))
+    # img_0 = convert16_2_8(os.path.join(imgs_dir, img_names[0]))
+    img_0 = cv2.cvtColor(cv2.imread(os.path.join(imgs_dir, img_names[0])), cv2.COLOR_RGB2HSV)[..., -1]
     final_img = np.zeros((img_0.shape[0], img_0.shape[1], 3), dtype=np.uint8)
     final_img[..., 0] = img_0
     for i in range(1, len(img_names)):
-        img_tmp = convert16_2_8(os.path.join(imgs_dir, img_names[i]))
+        # img_tmp = convert16_2_8(os.path.join(imgs_dir, img_names[i]))
+        # img_tmp = cv2.cvtColor(tifffile.imread(os.path.join(imgs_dir, img_names[i])), cv2.COLOR_RGB2GRAY)
+        img_tmp = cv2.cvtColor(cv2.imread(os.path.join(imgs_dir, img_names[i])), cv2.COLOR_RGB2HSV)[..., -1]
         final_img[..., i] = img_tmp
     tifffile.imwrite(imgs_dir + ".tif", final_img, compression='LZW')
     return imgs_dir + ".tif"
@@ -49,7 +54,8 @@ if __name__ == '__main__':
         raise Exception(f"Error input: {pixes}, {FOV_shape}")
 
     # 如果给的是图片文件夹，则需要将各个通道灰度图先合成一张RGB
-    img_path = merge_gray_to_RGB(img_path)
+    if os.path.isdir(img_path):
+        img_path = merge_gray_to_RGB(img_path)
 
     new_size = np.asarray(pixes) * FOV_shape
     print(pixes)
@@ -57,7 +63,8 @@ if __name__ == '__main__':
     print(new_size)
 
     # img_ori = cv2.cvtColor(cv2.imread(img_path), cv2.COLOR_BGR2RGB)
-    img_ori = np.asarray(Image.open(img_path))
+    # img_ori = np.asarray(Image.open(img_path))
+    img_ori = tifffile.imread(img_path)
     img_new = np.rot90(img_ori, -1)
     img_new = cv2.resize(img_new, new_size)
 
--- select_rect.py
+++ select_rect.py
@@ -1,5 +1,6 @@
 import argparse
 import copy
+import math
 import os.path
 import random
 # import sys
@@ -9,8 +10,28 @@ import cv2
 import numpy as np
 
 
+def rotate_rect(center_x_y, half_width, half_height, angle):
+    center_x, center_y = center_x_y
+    cos_a = math.cos(angle*math.pi/180)
+    sin_a = math.sin(angle*math.pi/180)
+
+    top_right_x = int(center_x + half_width * cos_a - half_height * sin_a)
+    top_right_y = int(center_y + half_width * sin_a + half_height * cos_a)
+
+    top_left_x = int(center_x - half_width * cos_a - half_height * sin_a)
+    top_left_y = int(center_y - half_width * sin_a + half_height * cos_a)
+
+    bot_left_x = int(center_x - half_width * cos_a + half_height * sin_a)
+    bot_left_y = int(center_y - half_width * sin_a - half_height * cos_a)
+
+    bot_right_x = int(center_x + half_width * cos_a + half_height * sin_a)
+    bot_right_y = int(center_y + half_width * sin_a - half_height * cos_a)
+
+    return [(top_left_x, top_left_y), (top_right_x, top_right_y), (bot_right_x, bot_right_y), (bot_left_x, bot_left_y)]
+
+
 def show_img(pic, name=None, line_width=3):
-    global rect_x, rect_y
+    global rect_x, rect_y, angle
     def mouse(event, x, y, flags, param):
         if event == cv2.EVENT_LBUTTONDOWN:
             print(x, y, pic[y, x])
@@ -19,9 +40,16 @@ def show_img(pic, name=None, line_width=3):
             cv2.waitKey(300)
             cv2.destroyWindow(name)
         if event == cv2.EVENT_MOUSEMOVE:
-            # n_pic = cv2.line(copy.copy(pic), (x, 0), (x, pic.shape[0]), (0, 255, 0), line_width)
-            n_pic = cv2.rectangle(copy.copy(pic), (x-rect_x, y-rect_y), (x+rect_x, y+rect_y), (255, 255, 0), line_width)
-            # n_pic = cv2.line(n_pic, (0, y), (pic.shape[1], y), (0, 255, 0), line_width)
+            # # n_pic = cv2.line(copy.copy(pic), (x, 0), (x, pic.shape[0]), (0, 255, 0), line_width)
+            # n_pic = cv2.rectangle(copy.copy(pic), (x-rect_x, y-rect_y), (x+rect_x, y+rect_y), (255, 255, 0), line_width)
+            # # n_pic = cv2.line(n_pic, (0, y), (pic.shape[1], y), (0, 255, 0), line_width)
+            (top_left_x, top_left_y), (top_right_x, top_right_y), (bot_right_x, bot_right_y), (bot_left_x, bot_left_y) =\
+                rotate_rect((x, y), rect_x, rect_y, angle)
+            n_pic = copy.copy(pic)
+            cv2.line(n_pic, (int(top_right_x), int(top_right_y)), (int(top_left_x), int(top_left_y)), (255, 255, 0), line_width)
+            cv2.line(n_pic, (int(top_left_x), int(top_left_y)), (int(bot_left_x), int(bot_left_y)), (255, 255, 0), line_width)
+            cv2.line(n_pic, (int(bot_left_x), int(bot_left_y)), (int(bot_right_x), int(bot_right_y)), (255, 255, 0), line_width)
+            cv2.line(n_pic, (int(bot_right_x), int(bot_right_y)), (int(top_right_x), int(top_right_y)), (255, 255, 0), line_width)
             cv2.imshow(name, n_pic)
 
     if not name:
@@ -48,6 +76,8 @@ if __name__ == '__main__':
     parser.add_argument('--save_dir', type=str, help="save dir", required=True)
     parser.add_argument('-x', type=int, help="rect width in (0-1000) default=100", default=100)
     parser.add_argument('-y', type=int, help="rect height in (0-1000) default=100", default=100)
+    parser.add_argument('--angle', type=int, help="rect height in (-180 -- 180) default=0", default=0)
+    parser.add_argument('--prefix', type=str, help="project num", default=None)
     args = parser.parse_args()
     print(args)
 
@@ -56,9 +86,11 @@ if __name__ == '__main__':
     img_path = args.img_path
     npy_path = args.npy_path
     save_dir = args.save_dir
+    prefix = args.prefix
 
     rect_x = args.x
     rect_y = args.y
+    angle = -args.angle
 
     npy = np.load(npy_path)
 
@@ -69,11 +101,14 @@ if __name__ == '__main__':
         img[npy > 0] = 200
     img_scale = cv2.resize(img, (1000, 1000))
     center_y_x = show_img(img_scale)
+    center_x_y = center_y_x[::-1]
     print(center_y_x)
 
     # 画一个二值图
     mask_scale = np.zeros(img_scale.shape[:2], dtype=np.uint8)
-    mask_scale[center_y_x[0]-rect_y: center_y_x[0]+rect_y, center_y_x[1]-rect_x: center_y_x[1]+rect_x] = 255
+    # mask_scale[center_y_x[0]-rect_y: center_y_x[0]+rect_y, center_y_x[1]-rect_x: center_y_x[1]+rect_x] = 255
+    cnts = rotate_rect(center_x_y, rect_x, rect_y, angle)
+    cv2.drawContours(mask_scale, [np.asarray(cnts)], 0, 255, -1)
     # show_img(mask_scale)
 
     # 过滤细胞
@@ -94,6 +129,9 @@ if __name__ == '__main__':
     # 保存
     if not os.path.exists(save_dir):
         os.mkdir(save_dir)
-    save_path = os.path.join(save_dir, "filter_cells.txt")
+    if prefix is None:
+        save_path = os.path.join(save_dir, "filter_cells.txt")
+    else:
+        save_path = os.path.join(save_dir, f"{prefix}_filter_cells.txt")
     with open(save_path, 'w') as f:
         f.write("\n".join(filter_cell_index))
